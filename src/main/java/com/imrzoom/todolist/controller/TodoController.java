dc8abc15c653a7fa3c5c847fc75b3cc0e5e0a7e4
Igor <igor.nacimento51@gmail.com>
2024-01-16 12:06:54 -0300

    "Adding swagger and documenting application"

--- src/main/java/com/imrzoom/todolist/controller/TodoController.java
+++ src/main/java/com/imrzoom/todolist/controller/TodoController.java
@@ -3,6 +3,12 @@ package com.imrzoom.todolist.controller;
 import com.imrzoom.todolist.model.Todo;
 import com.imrzoom.todolist.model.TodoDTO;
 import com.imrzoom.todolist.service.TodoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +18,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("api/todos")
+@Tag(name = "Todos", description = "Endpoint for managing your Todo list")
 public class TodoController {
 
     private final TodoService service;
@@ -21,12 +28,40 @@ public class TodoController {
     }
 
     @GetMapping
+    @Operation(summary = "Finds all todos!", description = "Finds all todos",
+    tags = {"Todos"},
+    responses = {
+            @ApiResponse(description = "Success", responseCode = "200",
+            content = {
+                    @Content(
+                            mediaType = "application/json",
+                            array = @ArraySchema(schema = @Schema(implementation = Todo.class))
+                    )
+            }),
+            @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+            @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
+
+    })
     public ResponseEntity<List<Todo>> findAll(){
         List<Todo> todos = this.service.findAll();
 
         return ResponseEntity.ok().body(todos);
     }
 
+    @Operation(summary = "Create a todo and list all of them!", description = "Create a todo and list all of them",
+            tags = {"Todos"},
+            responses = {
+                    @ApiResponse(description = "Created", responseCode = "201",
+                            content = {
+                                    @Content(
+                                            mediaType = "application/json",
+                                            array = @ArraySchema(schema = @Schema(implementation = Todo.class))
+                                    )
+                            }),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
+
+            })
     @PostMapping
     public ResponseEntity<List<Todo>> create(@RequestBody @Valid TodoDTO dto){
         List<Todo> todos = this.service.create(dto);
@@ -35,6 +70,20 @@ public class TodoController {
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Updates a todo and list all of them!", description = "Updates a todo and list all of them",
+            tags = {"Todos"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200",
+                            content = {
+                                    @Content(
+                                            mediaType = "application/json",
+                                            array = @ArraySchema(schema = @Schema(implementation = Todo.class))
+                                    )
+                            }),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
+
+            })
     public ResponseEntity<List<Todo>> update(@PathVariable Long id, @RequestBody TodoDTO dto){
         List<Todo> todos = this.service.update(id, dto);
 
@@ -42,6 +91,21 @@ public class TodoController {
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Deletes a todo and list all of them!", description = "Deletes a todo and list all of them",
+            tags = {"Todos"},
+            responses = {
+                    @ApiResponse(description = "No content", responseCode = "204",
+                            content = {
+                                    @Content(
+                                            mediaType = "application/json",
+                                            array = @ArraySchema(schema = @Schema(implementation = Todo.class))
+                                    )
+                            }),
+                    @ApiResponse(description = "Not found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
+
+            })
     public ResponseEntity<List<Todo>> delete(@PathVariable Long id){
         this.service.delete(id);
         return ResponseEntity.noContent().build();
